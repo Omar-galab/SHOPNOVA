1311f5fa9459db45789657d3dfa80a8a7c187d66
Omar-galab <omar.mahmoud2411@gmail.com>
2026-03-07 22:06:24 +0200

    feat: Add `addToWishlist` and `removeFromWishlist` product ID validators.

--- utils/validator/wishList.validator.js
+++ utils/validator/wishList.validator.js
@@ -17,15 +17,9 @@ export const addToWishlistValidator = [
     validatorMiddleware,
 ];
 export const removeFromWishlistValidator = [
-    param("productId")
-        .isMongoId()
-        .withMessage("Invalid product ID")
-        .custom((value) => {
-            return productModel.findById(value).then((product) => {
-                if (!product) {
-                    return Promise.reject(new Error("Product not found"));
-                }
-            });
-        }),
+    check("productId").notEmpty()
+    .withMessage("Product ID is required")
+    .isMongoId()
+    .withMessage("Invalid product ID format"),
     validatorMiddleware,
 ];
